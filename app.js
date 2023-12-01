712e39346d50a4bd1443ed08be15513d12e44f96
Ivanna Brigido Torres <110115815+ibrigido@users.noreply.github.com>
2023-12-01 18:46:30 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const kelvin = 0; 
+
+// i created a constant variable with the kelvin number 
+
+var celsius = kelvin - 273; 
+
+// i created a changing variable which called the celsius which did kelvin minus 273 
+
+var fahrenheit = celsius * (9/5) + 32
+
+// i created a variable for the fahrenheit that could change 
+
+var fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degress Fahrenheit.`);
